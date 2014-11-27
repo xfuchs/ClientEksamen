4135347bfdfec48f1da8ed26ae5e67dde9f59734
xfuchs <helena-fuchs@hotmail.com>
2014-11-27 15:14:59 +0100

    event add

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
- var year = new Date().getFullYear();
-  var month = new Date().getMonth();
-  var day = new Date().getDate();
 
-  var eventData = {
-    events : [
-      {'id':1, 'start': new Date(year, month, day, 12), 'end': new Date(year, month, day, 13, 35),'title':'Lunch with Mike'},
-      {'id':2, 'start': new Date(year, month, day, 14), 'end': new Date(year, month, day, 14, 45),'title':'Dev Meeting'},
-      {'id':3, 'start': new Date(year, month, day + 1, 18), 'end': new Date(year, month, day + 1, 18, 45),'title':'Hair cut'},
-      {'id':4, 'start': new Date(year, month, day - 1, 8), 'end': new Date(year, month, day - 1, 9, 30),'title':'Team breakfast'},
-      {'id':5, 'start': new Date(year, month, day + 1, 14), 'end': new Date(year, month, day + 1, 15),'title':'Product showcase'}
-    ]
-  };
+
+  var eventData = [];
+  
+        var eventsSession = $.sessionStorage.get("events");
+        if(eventsSession != null){
+            eventsSession.forEach(function(event){
+                var eventObj = {};
+                eventObj["id"] = event.eventid;
+                eventObj["start"] = new Date(event.strDateStart);
+                eventObj["end"] = new Date(event.strDateEnd)
+                eventObj["title"] = event.title;
+                eventData.push(eventObj);
+            });
+        }
+     
 
   $(document).ready(function() {
     $('#calendar').weekCalendar({
